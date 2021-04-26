a8efc2c32a0e28764f2177595acbe8ec0248fe1c
stalkerkos <78426220+stalkerkos@users.noreply.github.com>
2021-04-26 23:58:08 +0500

    lection9_dz4

--- first/src/main/java/FirstTest.java
+++ first/src/main/java/FirstTest.java
@@ -1,24 +1,25 @@
+import test.ship.StarShip;
+
 public class FirstTest {
 
     public static void main(String[] args) {
-        helloTest();
-        helloTest("username");
-        System.out.println(helloTest("значение1","значение2"));
+        //создаю экземпляр класса
+        StarShip ship = new StarShip();
+
+        //задаю конкретному кораблю конкретное свойство
+        ship.shipHeight = 253.25;
+        displayHeight(ship);
+
+
     }
 
-    /*демо методов*/
-    //1 метод без параметров
-    public static void helloTest() {
-        System.out.println("We are here! Hello!");
+    public static void displayHeight(StarShip ship) {
+        System.out.println("Высота корабля составляет : " + getStarShipHeight(ship));
+
     }
 
-    //2 метод с параметром
-    public static void helloTest(String value) {
-        System.out.println("We are here! Hello! " + " + "+value);
-    }
-    //3 метод с возвратом значения
-    public static String helloTest(String value1, String value2){
-        return "We are here! Hello! " + value1 +" , " + value2;
+    public static double getStarShipHeight(StarShip ship) {
+        return ship.shipHeight;
     }
 }
 
---
+++ first/src/main/java/test/ship/StarShip.java
@@ -0,0 +1,7 @@
+package test.ship;
+/*Created by Artur 04 2021*/
+
+public class StarShip {
+    /*высота*/
+    public double shipHeight;
+}
