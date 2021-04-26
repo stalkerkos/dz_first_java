9a03218a4a7bacd31b50bbcf3d9520783d55f47c
stalkerkos <78426220+stalkerkos@users.noreply.github.com>
2021-04-26 22:03:00 +0500

    Lection9_dz

--- first/src/main/java/FirstTest.java
+++ first/src/main/java/FirstTest.java
@@ -2,5 +2,15 @@ public class FirstTest {
 
 	public static void main (String[] args) {
 		System.out.println ("We are here! Hello! ");
+
+		System.out.println (8 + 2);
+		System.out.println (8 - 2);
+		System.out.println (8 * 2);
+		System.out.println (8 / 3);
+
+		System.out.println (8.0 / 3);
+
+		int side=7;
+		System.out.println ("Площадь квадрата со стороной "+side+ " = "+(side*side));
 	}
 }
